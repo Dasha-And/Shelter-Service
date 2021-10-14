c8babaed91f094f114cef20a17ad41b4c12a3f29
Dasha-And <dosados69@gmail.com>
2021-10-15 00:52:42 +0300

    removed hibernate annotations

--- src/main/java/shelter/service/model/User.java
+++ src/main/java/shelter/service/model/User.java
@@ -4,28 +4,24 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "users")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
-    private Integer id;
 
-    @Column(name = "username")
-    private String username;
+    private String name;
 
-    @Column(name = "password")
+    private String lastName;
+
+    private String phone;
+
+    private String email;
+
     private String password;
 
-    @Column(name = "role")
     private String role;
 
-    @Column(name = "shelter_id")
-    private Integer shelterId;
+    private String shelterId;
 
 }
