a37bb2abb0f420facc4cc6d39a415d38a57b05f2
Dasha-And <dosados69@gmail.com>
2021-10-15 00:54:53 +0300

    implemented insert and get methods for user using firebase in UserService

--- src/main/java/shelter/service/service/UserService.java
+++ src/main/java/shelter/service/service/UserService.java
@@ -1,24 +1,49 @@
 package shelter.service.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 import shelter.service.model.User;
-import shelter.service.repository.UserRepository;
 
-import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private static final String COLLECTION_NAME ="user";
 
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
+
+    public String insertUser(User user) throws ExecutionException, InterruptedException, FirebaseAuthException {
+        Firestore dbFirestore= FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionApiFuture=dbFirestore.collection(COLLECTION_NAME).document().set(user);
+
+        return collectionApiFuture.get().getUpdateTime().toString();
     }
 
-    public void insertUser(User user) {
-        userRepository.save(user);
+    public User getUserDetailsByEmail(String email) throws ExecutionException, InterruptedException {
+
+        Firestore dbFirestore= FirestoreClient.getFirestore();
+
+        Query query = dbFirestore.collection(COLLECTION_NAME).whereEqualTo("email", email);
+
+        ApiFuture<QuerySnapshot> future = query.get();
+
+        if (!future.get().getDocuments().isEmpty()) {
+            DocumentSnapshot document = future.get().getDocuments().get(0);
+
+            User user = null;
+            if(document.exists()) {
+                user = document.toObject(User.class);
+                return user;
+            }else{
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 }
