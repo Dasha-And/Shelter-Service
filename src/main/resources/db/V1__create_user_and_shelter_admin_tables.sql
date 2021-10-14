7fa11948d6f01fec74de9d051f963a767cadc75b
Dasha-And <dosados69@gmail.com>
2021-10-15 00:53:59 +0300

    deleted unnecessary files

--- src/main/resources/db/V1__create_user_and_shelter_admin_tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Users
-(
-    id serial PRIMARY KEY,
-    username character varying NOT NULL,
-    password character varying NOT NULL,
-    role character varying NOT NULL,
-    shelter_id int
-);
