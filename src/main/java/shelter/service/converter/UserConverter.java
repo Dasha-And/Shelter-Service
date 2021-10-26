2de4b061da72ee71f1df25d4ae7eb9a06d63c35d
Dasha-And <dosados69@gmail.com>
2021-10-26 23:57:38 +0300

    fixed role convertation

--- src/main/java/shelter/service/converter/UserConverter.java
+++ src/main/java/shelter/service/converter/UserConverter.java
@@ -9,7 +9,7 @@ public class UserConverter {
 
     public User toDomainModel(UserDto userDto) {
         String role = "admin";
-        if (userDto.getRole() == null) {
+        if (userDto.getRole() == null || userDto.getRole().equals("")) {
             role = "user";
         }
         return new User(userDto.getId(), userDto.getName(), userDto.getSurname(), userDto.getPhone(), userDto.getEmail(), userDto.getPassword(), role, userDto.getShelterId());
